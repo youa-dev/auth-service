b30aa6e44ff706b88b6a30611be96f817be353cc
Denis Onder <denis_onder@protonmail.com>
2020-04-28 19:31:15 +0200

    Tests working fine apparently

--- src/tests/Auth.test.ts
+++ src/tests/Auth.test.ts
@@ -12,7 +12,8 @@ describe("Authentication controller", () => {
       expect(res.data).to.include.all.keys(
         ...testingKeys.auth,
         "password",
-        "__v"
+        "__v",
+        "clientID"
       );
     });
   });
@@ -27,7 +28,7 @@ describe("Authentication controller", () => {
     it("should return all non-sensitive user information", async () => {
       const res = await apiTester("get", "/auth/me", null, token);
       expect(res.data).to.include.all.keys(
-        ...testingKeys.auth.map(v => v.replace("_id", "id"), "profile")
+        ...testingKeys.auth.map((v) => v.replace("_id", "id"), "profile")
       );
     });
   });
